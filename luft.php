a7c76199daa983f1f2b7ba04c49ea83f08bd7370
Malte Hübner <maltehuebner@gmx.org>
2017-12-20 15:46:25 +0100

    WIP for widget display.

--- luft.php
+++ luft.php
@@ -51,6 +51,28 @@ class LuftWidget extends WP_Widget
 
     function widget($args, $instance)
     {
+        extract( $args );
+        // these are the widget options
+        $title = apply_filters('widget_title', $instance['title']);
+        $text = $instance['text'];
+        $station = $instance['station'];
+
+        echo $before_widget;
+        // Display the widget
+        echo '<div class="widget-text wp_widget_plugin_box">';
+
+        // Check if title is set
+        if ( $title ) {
+            echo $before_title . $title . $after_title;
+        }
+
+        // Check if text is set
+        if( $station ) {
+            echo '<p class="wp_widget_plugin_text">'.$station.'</p>';
+        }
+
+        echo '</div>';
+        echo $after_widget;
     }
 }
 
