174e3f0935adce007904f1a3fc3cee1604611ad0
Malte Hübner <maltehuebner@gmx.org>
2017-12-20 15:33:28 +0100

    WIP.

--- luft.php
+++ luft.php
@@ -8,3 +8,25 @@ Author: Malte Hübner
 Author URI: https://maltehuebner.de
 License: MIT
 */
+
+class LuftWidget extends WP_Widget
+{
+    public function __construct()
+    {
+        parent::__construct('luft', 'Luft');
+    }
+
+    function form($instance)
+    {
+    }
+
+    function update($new_instance, $old_instance)
+    {
+    }
+
+    function widget($args, $instance)
+    {
+    }
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("LuftWidget");'));
